574744f61eb304e90f833a6e2c5a0ce272b9ba2f
Travis Linville <travislinville86@gmail.com>
2021-08-09 23:56:19 -0400

    Ch.2 complete

---
+++ ch2_variables/age.rb
@@ -0,0 +1,6 @@
+puts "How old are you?"
+age = gets.chomp
+puts "In 10 years, you will be " + (age.to_i + 10).to_s
+puts "In 20 years, you will be " + (age.to_i + 20).to_s
+puts "In 30 years, you will be " + (age.to_i + 30).to_s
+puts "In 40 years, you will be " + (age.to_i + 40).to_s
---
+++ ch2_variables/name.rb
@@ -0,0 +1,19 @@
+# Exercise 4 (getting full name)
+puts "What's your first name?"
+first_name = gets.chomp
+puts "What's your last name?"
+last_name = gets.chomp
+
+puts first_name + " " + last_name
+
+# Exercise 1 - commented out
+=begin
+puts "What is your name?"
+name = gets.chomp
+puts "Greetings " + name
+
+
+# Exercise 3 (name 10x)
+puts "Here's your name 10 times"
+10.times {puts name}
+=end
